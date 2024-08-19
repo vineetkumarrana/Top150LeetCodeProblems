3810450e63845dc4c7e05c1041aa386451c868ad
vineetkumarrana <108483824+vineetkumarrana@users.noreply.github.com>
2024-08-19 17:09:40 +0530

    Create 2keyskeyboard.cpp
    There is only one character 'A' on the screen of a notepad. You can perform one of two operations on this notepad for each step:
    
    Copy All: You can copy all the characters present on the screen (a partial copy is not allowed).
    Paste: You can paste the characters which are copied last time.
    Given an integer n, return the minimum number of operations to get the character 'A' exactly n times on the screen.

---
+++ 2keyskeyboard.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minSteps(int n) {
+//TC : O(√n)        
+     int ans=0; // (This will store the minimum number of steps)
+     //n= InputValue
+     for(int i=2;i*i<=n;){
+        if(n%i==0){
+            ans+=i;  // Add i to the answer (this corresponds to i paste operations after copying)
+            n/=i;  // Divide n by i to reduce the problem size
+        }
+        else {
+            i++;   // If i is not a divisor, try the next integer
+        }
+     }
+     if(n!=1){  // If n is still greater than 1 after the loop, it means n is prime or a prime factor remains
+        ans+=n;   // Add n to the answer, as we need n paste operations after one copy
+     }
+     return ans;
+    }
+};
